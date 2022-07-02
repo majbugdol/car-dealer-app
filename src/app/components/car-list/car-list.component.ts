e4778649fc3d0fa9399c0fb1d76edad4d746f82b
Marcin Czekajski <czekajskimarcin@gmail.com>
2022-07-02 23:59:00 +0200

    remove wrong import

--- src/app/components/car-list/car-list.component.ts
+++ src/app/components/car-list/car-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { CarsService } from 'src/app/services/cars.service';
-import { Car } from 'src/app/services/cars.service';
 
 @Component({
   selector: 'app-car-list',
