9fd2a37006d959b82ed3cc18562d162468c70ec2
Monika Bugdol <majbugdol@gmail.com>
2022-07-23 15:24:05 +0200

    Simple car form

--- src/app/layout/aside/aside.component.ts
+++ src/app/layout/aside/aside.component.ts
@@ -1,39 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CarsService, CarToPost } from 'src/app/services/cars.service';
+
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.scss'],
 })
 export class AsideComponent implements OnInit {
-  private carsToAdd: CarToPost[] = [
-    {
-      brand: 'Toyota',
-      model: 'Yaris',
-    },
-    {
-      brand: 'Toyota',
-      model: 'Corolla',
-    },
-    {
-      brand: 'Hyundai',
-      model: 'Getz',
-    },
-    {
-      brand: 'Ford',
-      model: 'Focus',
-    },
-    {
-      brand: 'Dacia',
-      model: 'Logan',
-    },
-  ];
+  // private carsToAdd: CarToPost[] = [
+  //   {
+  //     brand: 'Toyota',
+  //     model: 'Yaris',
+  //   },
+  //   {
+  //     brand: 'Toyota',
+  //     model: 'Corolla',
+  //   },
+  //   {
+  //     brand: 'Hyundai',
+  //     model: 'Getz',
+  //   },
+  //   {
+  //     brand: 'Ford',
+  //     model: 'Focus',
+  //   },
+  //   {
+  //     brand: 'Dacia',
+  //     model: 'Logan',
+  //   },
+  // ];
 
   constructor(private carsService: CarsService) {}
 
-  addCar() {
-    let i = Math.floor(Math.random() * this.carsToAdd.length);
-    this.carsService.addCarToList(this.carsToAdd[i]);
+  onSubmit(event: any) {
+    const carToAdd: CarToPost = {
+      brand: event.target.brand.value,
+      model: event.target.model.value,
+    };
+    console.log(carToAdd);
+    this.addCar(carToAdd);
+  }
+  addCar(carToAdd: CarToPost) {
+    this.carsService.addCarToList(carToAdd);
   }
   ngOnInit(): void {}
 }
