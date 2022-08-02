154c38e11cb9462b0880e6202f982e68aeca829c
majbugdol <majbugdol@gmail.com>
2022-08-02 11:44:59 +0200

    added disabled button

--- src/app/components/form-add-car/form-add-car.component.ts
+++ src/app/components/form-add-car/form-add-car.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { CarsService, CarToPost } from '../services/cars.service';
+import { CarsService, CarToPost } from '../../services/cars.service';
 
 @Component({
   selector: 'app-form-add-car',
