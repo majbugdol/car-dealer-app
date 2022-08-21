8b59b6bb3061748ab0563a7d123b2cd33707b306
majbugdol <majbugdol@gmail.com>
2022-08-21 19:38:28 +0200

    adds interface and validators to form

--- src/app/components/form-log-in/form-log-in.component.ts
+++ src/app/components/form-log-in/form-log-in.component.ts
@@ -1,27 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, Form } from '@angular/forms';
 
+interface ILoginForm {
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
 @Component({
   selector: 'app-form-log-in',
   templateUrl: './form-log-in.component.html',
   styleUrls: ['./form-log-in.component.scss'],
 })
 export class FormLogInComponent implements OnInit {
-  logIn = new FormGroup({
-    email: new FormControl('', Validators.required),
-    password: new FormControl('', Validators.required),
+  logIn: FormGroup<ILoginForm> = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+      nonNullable: true,
+    }),
+    password: new FormControl('', {
+      validators: Validators.required,
+      nonNullable: true,
+    }),
   });
 
-  // getErrorMessage() {
-  //   if (this.logIn.hasError('required')) {
-  //     return 'Pole nie może być puste';
-  //   }
+  public get controls(): ILoginForm {
+    return this.logIn.controls;
+  }
 
-  //   return this.logIn.hasError('email') ? 'Niepoprawny format e-mail' : '';
-  // }
+  getErrorMessageEmail() {
+    if (this.controls.email.hasError('required')) {
+      return 'Pole nie może być puste';
+    }
+    return this.controls.email.hasError('email')
+      ? 'Niepoprawny format e-mail'
+      : '';
+  }
+
+  getErrorMessagePassword() {
+    return 'Pole nie może być puste';
+  }
 
   hide = true;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.logIn);
+  }
 }
