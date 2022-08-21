6d8fe525f4df7e8650ff55d50d7ab908713b68d0
majbugdol <majbugdol@gmail.com>
2022-08-21 20:47:47 +0200

    adds users service methods

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface ILoginToPost {
+  email: string;
+  password: string;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
+  public login(loginData: ILoginToPost) {
+    this.postLoginData(loginData).subscribe((res) => console.log(res));
+  }
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  private postLoginData(loginData: ILoginToPost) {
+    return this.http.post(
+      'https://car-dealer-backend.herokuapp.com/users/login',
+      loginData
+    );
+  }
 }
